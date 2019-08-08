9337c4192fd96f7560adba5dc0ff2d3514857358
AlexHoratio <horatioesf@protonmail.com>
2019-08-08 15:07:18 +0100

    bunch of fuck

---
+++ Scripts/Buttons/start.gd
@@ -0,0 +1,16 @@
+extends TextureButton
+
+var target_position = Vector2(0, 0)
+
+func _ready():
+	pass
+	
+func _process(delta):
+	rect_position = rect_position.linear_interpolate(target_position, 0.1)
+	$shadow.position = $shadow.position.linear_interpolate(target_position, 0.1)
+
+func _on_start_mouse_entered():
+	target_position.x = -20
+
+func _on_start_mouse_exited():
+	target_position.x = 0
