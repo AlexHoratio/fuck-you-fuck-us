222bfdf6c5608768eebf5e338c0b09d7b2d9d56c
AlexHoratio <horatioesf@protonmail.com>
2019-08-09 00:43:28 +0100

    lemme commit before i make a mess of everything

--- Scripts/Buttons/sfx.gd
+++ Scripts/Buttons/sfx.gd
@@ -2,6 +2,20 @@ extends TextureButton
 
 var target_position = Vector2(100, 10)
 
+var textures = {
+	"unmuted":[
+		preload("res://Graphics/Menu/Buttons/SFX/normal.png"),
+		preload("res://Graphics/Menu/Buttons/SFX/shadow.png"), 
+	],
+	"muted":[
+		preload("res://Graphics/Menu/Buttons/SFX/disabled/normal.png"),
+		preload("res://Graphics/Menu/Buttons/SFX/disabled/shadow.png"), 
+	]
+}
+var muted = false
+var trauma = 0
+var MAX_SHAKE = 8
+
 func _ready():
 	pass
 	
@@ -9,11 +23,23 @@ func _process(delta):
 	rect_position = rect_position.linear_interpolate(target_position, 0.1)
 	$shadow.position = $shadow.position.linear_interpolate(target_position - Vector2(100, 10), 0.1)
 
-func _on_quit_pressed():
-	get_tree().quit()
-
+	trauma = clamp(trauma - delta*3, 0, 1)
+	$shadow.offset = Vector2((2*randf()-1)*MAX_SHAKE, (2*randf()-1)*MAX_SHAKE) * trauma * trauma
+	
 func _on_sfx_mouse_entered():
 	target_position.x = 90
 
 func _on_sfx_mouse_exited():
 	target_position.x = 100
+
+func _on_sfx_pressed():
+	muted = not(muted)
+	
+	if muted:
+		texture_normal = textures["muted"][0]
+		get_node("shadow").texture = textures["muted"][1]
+	else:
+		texture_normal = textures["unmuted"][0]
+		get_node("shadow").texture = textures["unmuted"][1]
+		
+	trauma = 1
